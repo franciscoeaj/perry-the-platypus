3750352f7315ae8f02fb3f29f935bc49c01991d7
Francisco Júnior <franciscojunior@me.com>
2019-03-04 02:25:27 -0300

    so come lets watch the rain as its fallin down

--- api/server.js
+++ api/server.js
@@ -3,22 +3,29 @@ require("dotenv").config();
 const Koa = {
   Instance: require("koa"),
   Router: require("koa-router"),
-  Logger: require("koa-logger")
+  Logger: require("koa-logger"),
+  BodyParser: require("koa-bodyparser")
 };
 
 const app = new Koa.Instance();
 const router = new Koa.Router();
+const axios = require("axios");
 const port = process.env.PORT || 3000;
 
 app.use(Koa.Logger());
+app.use(Koa.BodyParser());
 
 router.get("/", (ctx, next) => {
   ctx.body = "Hello World!";
 });
 
 router.post("/notification/listener", (ctx, next) => {
-  console.log(ctx.request);
-  ctx.body = "POST req made successfully";
+  // verify if its form-urlencoded and from which url the request has been made
+  if (ctx.is("application/x-www-form-urlencoded")) {
+    // TODO: make request to api given the received transaction code and handle the returned data
+  } else {
+    ctx.body = "Invalid request";
+  }
 });
 
 app.use(router.routes());
