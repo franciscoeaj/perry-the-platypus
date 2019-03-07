8da6679dcb2fbe97dafb09fc2f22945400955957
Francisco Júnior <franciscojunior@me.com>
2019-03-07 00:06:13 -0300

    less/less-loader and babel-plugin-import

--- web/src/main.js
+++ web/src/main.js
@@ -1,7 +1,6 @@
 import Vue from "vue";
 import App from "./App.vue";
-import Button from "ant-design-vue/lib/button";
-import "ant-design-vue/dist/antd.css";
+import { Button } from "ant-design-vue";
 
 Vue.component(Button.name, Button);
 
