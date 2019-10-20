55526a2ab516707c25fae113b2fa15c546785cbe
Francisco E. Júnior <franciscojunior@me.com>
2019-10-20 15:08:25 -0300

    changing from mariadb to pgsql

--- api/src/config/Database.js
+++ api/src/config/Database.js
@@ -4,7 +4,7 @@ const Sequelize = require('sequelize')
 
 const database = new Sequelize(
   process.env.DB_NAME || 'perry',
-  process.env.DB_USER || 'root',
+  process.env.DB_USER || 'perry',
   process.env.DB_PASS || 'ultrasecretpassword',
   {
     host: process.env.DB_HOST || 'localhost', 
