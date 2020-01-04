4f36f2d0d08a774d2164e7f9dca04f5c1e90f135
Francisco E. Júnior <franciscojunior@me.com>
2020-01-04 00:20:37 -0300

    finishing model

--- api/src/models/Gateway.js
+++ api/src/models/Gateway.js
@@ -1,5 +1,21 @@
+const connector = require('../config/Database')
 const Sequelize = require('sequelize')
 
 class Gateway extends Sequelize.Model {}
+
+Gateway.init({
+  id: {
+    type: Sequelize.INTEGER,
+    primaryKey: true,
+    autoIncrement: true
+  },
+  name: {
+    type: Sequelize.STRING(60),
+    allowNull: false
+  }
+}, {
+  sequelize: connector,
+  modelName: 'gateways'
+})
 
 module.exports = Gateway
